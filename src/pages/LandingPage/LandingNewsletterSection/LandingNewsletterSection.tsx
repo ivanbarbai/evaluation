f80663d3edcd731a1a399e0fb3ba956ac776c71f
Ivan Barba <ivanbarba@canesstudio.com>
2021-02-26 14:10:33 -0600

    Adding newsletter section

--- src/pages/LandingPage/LandingNewsletterSection/LandingNewsletterSection.tsx
+++ src/pages/LandingPage/LandingNewsletterSection/LandingNewsletterSection.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Row, Col, Typography, Input, Space } from 'antd';
+import { MailOutlined } from '@ant-design/icons';
+
+import './styles.less'
+
+const { Text, Title } = Typography;
 
 export default function LandingNewsletterSection() {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Row gutter={16} className='top-row color-5 bg-news'>
+                <Col span={12} className='justify' >
+                    <Title level={3} className='uppercase'>Newsletter</Title>
+                    <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid voluptates natus aliquam porro esse, vero odio eligendi asperiores sunt dolor, voluptatibus ea id.</Text>
+                </Col>
+                <Col span={12} className='justify' >
+                    <Space direction='vertical'>
+                        <Input className='email-news' placeholder='Escriba su Email' suffix={<MailOutlined />} />
+                    </Space>
+                </Col>
+            </Row>
+        </>
     )
 }
